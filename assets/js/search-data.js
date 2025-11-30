67800af83485199deb4529521d2e2ac217300d76
scraed <czhengac@connect.ust.hk>
2025-11-30 16:54:51 +0000

    Deploying to gh-pages from @ scraed/2026@ce19af5610d79418b46a20af393bfdf5eff0c082 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -303,11 +303,11 @@ ninja.data = [{
             window.location.href = "/2026/blog/2026/distill-example/";
           
         },
-      },{id: "post-sample-blog-post",
+      },{id: "post-rethinking-the-diffusion-model-from-a-langevin-perspective",
         
-          title: "Sample Blog Post",
+          title: "Rethinking the Diffusion Model from a Langevin Perspective",
         
-        description: "Your blog post&#39;s abstract. Please add your abstract or summary here and not in the main body of your text. Do not include math/latex or hyperlinks.",
+        description: "Diffusion models are often introduced from multiple perspectives—such as VAEs, score matching, or flow matching—accompanied by dense and technically demanding mathematics that can be difficult for beginners to grasp. This article offers a fresh Langevin perspective on diffusion models to lower the technical barrier, aiming to present diffusion models in a simpler, clearer, and more intuitive way while addressing the following questions: 1. How does the reverse process invert the forward process to generate data from pure noise? 2. How can ODE-based and SDE-based diffusion models be unified under a single framework? 3. Why are diffusion models theoretically superior to ordinary VAEs? 4. How can Denoising, Score Matching, and Flow Matching training objectives be unified and derived from first principles? We demonstrate that the Langevin perspective offers clear and straightforward answers to these questions, providing pedagogical value for both learners and experienced researchers seeking deeper intuition.",
         section: "Posts",
         handler: () => {
           
